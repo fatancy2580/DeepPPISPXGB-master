8659ec1b80f28ec032066aad7dcf093c51077891
fatancy2580 <52057084+fatancy2580@users.noreply.github.com>
2021-05-23 21:30:29 +0800

    Update data_generator.py

--- generator/data_generator.py
+++ generator/data_generator.py
@@ -21,7 +21,7 @@ class dataSet(data.Dataset):
         super(dataSet,self).__init__()
 
 
-        #读取五个文件
+     
         self.all_sequences = []
         for seq_file in sequences_file:
             with open(seq_file,"rb") as fp_seq:
@@ -57,23 +57,23 @@ class dataSet(data.Dataset):
 
         
 
-    def __getitem__(self,index):   #让对象实现迭代功能
+    def __getitem__(self,index):   
         
-        count,id_idx,ii,dset,protein_id,seq_length = self.protein_list[index] #protein_list保存的是所有的数据
+        count,id_idx,ii,dset,protein_id,seq_length = self.protein_list[index] 
         window_size = self.window_size
         id_idx = int(id_idx)
-        win_start = ii - window_size   #开始的索引
-        win_end = ii + window_size     #终止的索引
+        win_start = ii - window_size  
+        win_end = ii + window_size    
         seq_length = int(seq_length)
-        label_idx = (win_start+win_end)//2   #标签的索引为中间位置
+        label_idx = (win_start+win_end)//2   
 
 
-        #全局特征的生成办法
+        
         all_seq_features = []
         seq_len = 0
-        for idx in self.all_sequences[id_idx][:self.max_seq_len]:  #最后蛋白质序列通过embedding转为稀疏向量
-            acid_one_hot = [0 for i in range(20)] #长度为20的额全零列表
-            acid_one_hot[idx] = 1  #序列采用one-hot编码
+        for idx in self.all_sequences[id_idx][:self.max_seq_len]:  
+            acid_one_hot = [0 for i in range(20)] 
+            acid_one_hot[idx] = 1  
             all_seq_features.append(acid_one_hot)
             seq_len += 1
         while seq_len<self.max_seq_len:
@@ -81,24 +81,24 @@ class dataSet(data.Dataset):
             all_seq_features.append(acid_one_hot)
             seq_len += 1
 
-        all_pssm_features = self.all_pssm[id_idx][:self.max_seq_len]  #pssm采用密度向量
+        all_pssm_features = self.all_pssm[id_idx][:self.max_seq_len] 
         seq_len = len(all_pssm_features)
         while seq_len<self.max_seq_len:
             zero_vector = [0 for i in range(20)]
             all_pssm_features.append(zero_vector)
             seq_len += 1
 
-        all_dssp_features = self.all_dssp[id_idx][:self.max_seq_len] #为什么没用ont-hot因为本来就是one_hot
+        all_dssp_features = self.all_dssp[id_idx][:self.max_seq_len]
         seq_len = len(all_dssp_features)
         while seq_len<self.max_seq_len:
-            zero_vector = [0 for i in range(9)]  #长度不够后面用全零数组添加
+            zero_vector = [0 for i in range(9)] 
             all_dssp_features.append(zero_vector)
             seq_len += 1
         
-        #其作用就是生成局部特征
+        
         local_features = []
         labels = []
-        while win_start<0:  #前面和后面两部分进行填充直到第window_size为止
+        while win_start<0: 
             data = []
             acid_one_hot = [0 for i in range(20)]
             data.extend(acid_one_hot)
@@ -112,7 +112,7 @@ class dataSet(data.Dataset):
             local_features.extend(data)
             win_start += 1
        
-        valid_end = min(win_end,seq_length-1) #最后window_size部分取其中的最小值
+        valid_end = min(win_end,seq_length-1) 
         while win_start<=valid_end:
             data = []
             idx = self.all_sequences[id_idx][win_start]
@@ -149,12 +149,12 @@ class dataSet(data.Dataset):
             win_start += 1
 
 
-        label = self.all_label[id_idx][label_idx] #第id_idx序列的第label_idex个标签
+        label = self.all_label[id_idx][label_idx] 
         label = np.array(label,dtype=np.float32)
 
-        all_seq_features = np.stack(all_seq_features)#将得到的特征默认横向堆叠在一起
-        all_seq_features = all_seq_features[np.newaxis,:,:] #np.newaxis表示增加一个坐标轴，二维变三维
-        all_pssm_features = np.stack(all_pssm_features)#将得到的数组进行组合
+        all_seq_features = np.stack(all_seq_features)
+        all_seq_features = all_seq_features[np.newaxis,:,:] 
+        all_pssm_features = np.stack(all_pssm_features)
         all_pssm_features = all_pssm_features[np.newaxis,:,:]
 
         all_dssp_features = np.stack(all_dssp_features)
@@ -162,13 +162,13 @@ class dataSet(data.Dataset):
         local_features = np.stack(local_features)
 
         
-        #这里得到的数据是三维的
+        
         return all_seq_features,all_pssm_features,all_dssp_features,local_features,label
         # return  local_features, label
 
 
                 
 
-    def __len__(self):   #其作用是确定__getitem__的长度
+    def __len__(self):   
 
         return len(self.protein_list)
