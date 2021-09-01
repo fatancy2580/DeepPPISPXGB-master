3b666c810ec7b7264e669493232c980b568a832a
fatancy2580 <52057084+fatancy2580@users.noreply.github.com>
2021-09-01 15:28:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -54,39 +54,31 @@ def calculateEvaluationMetrics(x_train, y_train,x_test,y_test,pos_label=1):
 
 def plotAUPRCandAUROCcurce_table(x_train, y_train, x_test, y_test):
     sn, sp, prec, acc, F1, mcc, AUROC, AUPRC, fpr, tpr, precision, recall = calculateEvaluationMetrics(x_train, y_train,x_test,y_test)
-    sn_v, sp_v, prec_v, acc_v, F1_v, mcc_v, AUROC_v, AUPRC_v, fpr_v, tpr_v, precision_v, recall_v  = calculateEvaluationMetrics(x_train, y_train,valid_feature,valid_label)
 
     fig = plt.figure(figsize=(10, 4))
     ax1 = fig.add_subplot(121, xlabel="recall", ylabel='precision', title="Precision-Recall curve")
     ax1.plot(recall, precision, color='lawngreen', label='AUPRC_test = %0.3f' % AUPRC)
-    ax1.plot(recall_v, precision_v, color='deepskyblue', label='AUPRC_valid = %0.3f' % AUPRC_v)
     ax1.legend(loc='lower right')
     ax1.plot([0, 1], [1, 0], 'r--')
 
-    ax2 = fig.add_subplot(122, title='ROC curve', ylabel='True Positive Rate', xlabel='False Positive Rate')
+    ax2 = fig.add_subplot(122,  xlabel='False Positive Rate',ylabel='True Positive Rate',title='ROC curve')
     ax2.plot(fpr, tpr, color='lawngreen', label='AUR0C_test = %0.3f' % AUROC)
-    ax2.plot(fpr_v, tpr_v, color='deepskyblue', label='AUR0C_valid = %0.3f' % AUROC_v)
     ax2.legend(loc='lower right')
     ax2.plot([0, 1], [0, 1], 'r--')
     plt.savefig('AUROC&AUPRC')
     plt.show()
-    #绘制表格
+    
     x = PrettyTable(["validation_mode", "ACC", "Precision", "Recall", "F1", "MCC", "Specificity", "AUROC", "AUPRC"])
     x.add_row(["independence_test", acc, prec, sn, F1, mcc, sp, AUROC, AUPRC])
-    x.add_row(["independence_valid", acc_v, prec_v, sn_v, F1_v, mcc_v, sp_v, AUROC_v, AUPRC_v])
     print(x)
 
 if __name__ == '__main__':
     file1 = sys.argv[1]
     file2 = sys.argv[2]
-    file3 = sys.argv[3]
     protein_train = pd.read_csv(file1)
     xtrain = protein_train.iloc[:, 1:1028]
     ytrain = protein_train.iloc[:, -1]
     protein_test = pd.read_csv(file2)
     xtest = protein_test.iloc[:, 1:1028]
     ytest = protein_test.iloc[:, -1]
-    protein_valid = pd.read_csv(file3)
-    valid_feature = protein_valid.iloc[:, 1:1028]
-    valid_label = protein_valid.iloc[:, -1]
     plotAUPRCandAUROCcurce_table(xtrain, ytrain, xtest, ytest)
