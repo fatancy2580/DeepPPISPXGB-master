c8d04d21e56628da1254e73a53442392900e2455
fatancy2580 <52057084+fatancy2580@users.noreply.github.com>
2021-10-04 16:11:12 +0800

    Update UMAP_visualization_preprocessing_feature.py

--- visualization/UMAP_visualization_preprocessing_feature.py
+++ visualization/UMAP_visualization_preprocessing_feature.py
@@ -10,7 +10,7 @@ import  seaborn as sns
 import numpy as np
 feature_path = sys.argv[1]
 protein_train = pd.read_csv(feature_path)
-xtrain = protein_train.iloc[:, 1:-1]
+xtrain = protein_train.iloc[:, 1:-1] 
 ytrain = protein_train.iloc[:, -1]
 ytrain = pd.DataFrame(ytrain)
 
@@ -20,9 +20,9 @@ for index,value in ytrain.iterrows():
     arr = np.array(value)[0]
     ls.append(dic[arr])
 
+
 embedding_p = umap.UMAP().fit_transform(xtrain)
 fig = plt.figure(figsize=(5, 4))
-print(embedding_p[:, 0].shape)
 ax1 = fig.add_subplot(111, xlabel="UMAP1", ylabel='UMAP2', title="Preprocessing features of training data set")
 sns.scatterplot(embedding_p[:, 0],embedding_p[:, 1],hue=ls,palette="pastel",sizes=10)
 ax1.legend(loc="lower right")
